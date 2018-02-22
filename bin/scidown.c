0ccb87a1833405755f01be84d4de26f4225f1318
Martino Ferrari <manda.mgf@gmail.com>
2018-02-22 22:49:38 +0100

    better memeory check and grow

--- bin/scidown.c
+++ bin/scidown.c
@@ -4,15 +4,22 @@
 int main(int    argc,
          char **argv)
 {
+	size_t n;
+	size_t i;
+
 	/**Initial string**/
 	dynstr* str = dynstr_from("Hi, how are you?\nFine thanks, and you?\nI'm good!");
 	printf("Original size %lu\n", str->size);
 
-    /**Split in lines**/
-	size_t n;
-	dynstr** lines = dynstr_splits(str, "\n", &n);
+	/**Look for a string**/
+	dyniter* pos = dynstr_match_all(str, "\n", &n);
+	for (i = 0; i < n; i++)
+	{
+		printf("- %s\n", dyniter_print(pos[i]));
+	}
+	                                  /**Split in lines**/
+	dynstr** lines = dynstr_splitc(str, '\n', &n);
 	printf("%lu lines\n", n);
-	size_t i;
 	for (i = 0; i < n; i++)
 	{
 		printf("> %s\n", lines[i]->data);
